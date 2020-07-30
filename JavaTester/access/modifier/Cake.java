db45b640dc0b29f936567dddad9907f44f1eb880
Quintus <dangquynh.1412@gmail.com>
2020-07-30 20:42:33 +0700

    commit

---
+++ JavaTester/access/modifier/Cake.java
@@ -0,0 +1,24 @@
+package access.modifier;
+
+public class Cake {
+	private String banhLoaiMot = "Platinum";
+	public String banhLoaiHai = "Gold";
+	String banhLoaiBa = "Silver"; 
+	protected String banhLoaiBon= "Low";
+	
+	private void setLoaiBanh() {
+		
+	}
+	
+	public void setMauBanh() {
+		
+	}
+
+	void setKichThuoc () {
+		
+	}
+	
+	protected void setGiaBanh() {
+		
+	}
+}
---
+++ JavaTester/access/modifier/NhanVien.java
@@ -0,0 +1,9 @@
+package access.modifier;
+
+public class NhanVien {
+
+	public static void main(String[] args) {
+		Cake banh = new Cake();
+		banh.
+	}
+}
